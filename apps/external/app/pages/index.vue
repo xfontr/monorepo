b249c88b340bac859b59c14b5603ab1a8078591a
Xifré Font <96349146+xfontr@users.noreply.github.com>
2026-07-03 15:10:14 +0200

    feat: Storybook, export tsconfigs, add docs

--- apps/external/app/pages/index.vue
+++ apps/external/app/pages/index.vue
@@ -1,11 +1,4 @@
-<script lang="ts" setup>
-import useUserStore from "~~/layers/user/app/stores/user";
-
-const userStore = useUserStore();
-</script>
-
 <template>
-    <p>{{ userStore.user?.email }}</p>
     <p>{{ $t("shared.health") }}</p>
     <p>{{ $t("meta.title") }}</p>
 </template>
--- packages/configs/src/eslint/lib/ignores.ts
+++ packages/configs/src/eslint/lib/ignores.ts
@@ -1,3 +1,3 @@
-const baseIgnores = ["**/dist", "**/coverage", "**/types", "**/*.d.ts", "*.config.ts"];
+const baseIgnores = ["**/dist", "**/coverage", "**/types", "**/*.d.ts", "*.config.ts", "**/storybook-static"];
 
 export default baseIgnores;
