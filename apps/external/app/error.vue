f7972f2305e0498196987712c89b5120f235a7a8
Xifré Font <xfont@dogfydiet.com>
2026-08-11 17:28:29 +0200

    feat: Implement custom nuxt i18n module

--- apps/external/app/error.vue
+++ apps/external/app/error.vue
@@ -1,3 +1,50 @@
+<script lang="ts" setup>
+import type { NuxtError } from "#app";
+
+const { error } = defineProps<{ error: NuxtError<{ message?: string }> }>();
+
+const isDev = import.meta.dev;
+
+useHead({
+    title: `Error ${error.status}`,
+    meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "robots", content: "noindex" },
+    ],
+});
+</script>
+
 <template>
-    <p>Error</p>
+    <main class="error">
+        <span class="error__status">
+            {{ error.status }}
+        </span>
+
+        <h1 class="error__message">
+            {{ error.message }}
+        </h1>
+
+        <section
+            v-if="isDev"
+            class="error__debug"
+        >
+            <details
+                v-if="error.data?.message"
+                open
+            >
+                <summary>Message</summary>
+                <p>{{ error.data.message }}</p>
+            </details>
+
+            <details v-if="error.data">
+                <summary>Data</summary>
+                <p>{{ error.data }}</p>
+            </details>
+
+            <details>
+                <summary>Stack</summary>
+                <pre>{{ error.stack }}</pre>
+            </details>
+        </section>
+    </main>
 </template>
